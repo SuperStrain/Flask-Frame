d604e89a52b33e4fe9810987e38b39b73507c371
SuperStrain <1143124497@qq.com>
2022-05-19 10:44:33 +0800

    5月19日基础功能实现

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask
+#总共要学两步，第一步定义好路径，第二步利用render_template返回html文件
+
+
+from flask import Flask,render_template #渲染模板
 #debug模式的开启
 
 app = Flask(__name__)
 
-#路由解析，通过用户访问的路径，匹配相应的函数
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
 
-@app.route('/index')
-def hello():
-    return "您好"
+#通过访问路径，获得用户的字符串参数
+@app.route('/user/<name>')
+def welcome1(name):
+    return "欢迎您%s"%name
+
+#通过访问路径，获得用户的整型参数（此为还有float类型）
+@app.route('/user/<int:id>')
+def welcome2(id):
+    return "您好，%d 号会员"%id
+
+
+@app.route('/')
+def tetx():
+    return render_template("text.html")
 
 if __name__ == '__main__':
     app.run()
