3e375466e7fe3e5cde2c7af25687c7d1b9277e83
SuperStrain <1143124497@qq.com>
2022-05-21 10:44:01 +0800

    5月21日 flask框架基础功能测试完成

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-#总共要学两步，第一步定义好路径，第二步利用render_template返回html文件
+#总共要学两步，
+# 第一步定义好路径，
+# 第二步利用render_template返回html文件
 
 
-from flask import Flask,render_template #渲染模板
+from flask import Flask,request,render_template#render_template为渲染模板
+import datetime
 #debug模式的开启
 
 app = Flask(__name__)
@@ -20,7 +23,22 @@ def welcome2(id):
 
 @app.route('/')
 def tetx():
-    return render_template("text.html")
+    time=datetime.date.today()  #普通变量
+    name=["张三","李四","王二麻子"] #列表类型
+    task={"任务":"打扫卫生","时间":"3个小时"}#字典类型
+    return render_template("text.html",var=time,list=name,task=task)
+
+#表单提交
+@app.route('/text/register')
+def register():
+    return render_template('text/register.html')
+
+#接受表单提交的路由需要指定method为post
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method=='POST':
+        result=request.form
+        return render_template('text/result.html',result=result)
 
 if __name__ == '__main__':
     app.run()
